35a8e471ba1f4560c647afe3695caab48b9084fc
Kristina Zakharova <zakharova.kr@gmail.com>
2019-12-18 21:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
   var lowercase = "hello!"
-  string.toUpperCase === uppercase;
-  string.toLowerCase === lowercase;
 
-  if (string === lowercase) {
+  if (string.toLowerCase === lowercase) {
     return "I can't hear you!";
-  } else if (string === uppercase) {
+  } else if (string.toUpperCase === uppercase) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
