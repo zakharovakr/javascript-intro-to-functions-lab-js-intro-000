61b1c754ea06e1402071ed8303b3e0f43451410b
Kristina Zakharova <zakharova.kr@gmail.com>
2019-12-18 21:46:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
-  var lowercase = "hello!"
+  var uppercase = "HELLO!";
+  var lowercase = "hello!";
 
   if (string.toLowerCase === lowercase) {
     return "I can't hear you!";
